db816912997cba880a05c39727b2b81897aa77ae
mischlecht <mischlecht@gmail.com>
2017-04-22 22:14:21 -0500

    Remove more fluff

--- src/app/routes.js
+++ src/app/routes.js
@@ -4,14 +4,12 @@ import { Route, IndexRoute } from 'react-router';
 import App from './app.jsx';
 import HomeController from '../home/home.controller.jsx';
 import ImagesController from '../images/images.controller.jsx';
-import AboutPage from '../about/components/about.jsx';
 import NotFoundPage from '../shared/components/not-found-page.jsx';
 
 export default (
   <Route path="/" component={App}>
     <IndexRoute component={HomeController}/>
     <Route path="images" component={ImagesController}/>
-    <Route path="about" component={AboutPage}/>
     <Route path="*" component={NotFoundPage}/>
   </Route>
 );
